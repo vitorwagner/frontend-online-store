3b2ba8d64af5e040cf3d7a6d3a5209cb4e5f938a
João Coutinho <joaocoutinhof@hotmail.com>
2022-11-09 16:58:47 -0300

    grupo-11-req-4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  state = {
+    category: [],
+  };
+
+  async componentDidMount() {
+    const test = await getCategories();
+    this.setState({
+      category: test,
+    });
+  }
+
   render() {
+    const { category } = this.state;
     return (
       <div data-testid="home-initial-message">
+        {
+          category.map((element) => (
+            <label
+              htmlFor="a"
+              key={ element.id }
+            >
+              <input
+                data-testid="category"
+                type="radio"
+              />
+
+              {element.name}
+
+            </label>
+          ))
+        }
         <label htmlFor="a">
           <input
             type="text"
