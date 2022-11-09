e061a30227b1d062ffa6f457c55f257fb39c67cb
Vitor Wagner <vitormwagner@gmail.com>
2022-11-09 16:40:00 -0300

    Requerimento 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/cart" component={ Cart } />
       </Switch>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import CartImage from '../images/shopping-cart.png';
 
 class Home extends Component {
   render() {
     return (
-      <div data-testid="home-initial-message">
-        <label htmlFor="a">
-          <input
-            type="text"
-          />
-        </label>
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </div>
+      <>
+        <div data-testid="home-initial-message">
+          <label htmlFor="a">
+            <input
+              type="text"
+            />
+          </label>
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>
+        <Link data-testid="shopping-cart-button" to="/cart">
+          <img src={ CartImage } alt="Carrinho de Compras" className="Cart-Icon" />
+        </Link>
+      </>
+
     );
   }
 }
