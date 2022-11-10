7cc5a11e50ca6801638ddc315ae45f6db0500ee6
Vitória <vitcs11@gmail.com>
2022-11-10 18:51:48 -0300

    Adiciona botão de adc ao carrinho

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 class CardProduct extends Component {
   render() {
-    const { title, thumbnail, price } = this.props;
+    const { title, thumbnail, price, handleClick } = this.props;
     return (
       <div data-testid="product">
         <p>{title}</p>
         <img src={ thumbnail } alt={ title } />
         <p>{ price }</p>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ handleClick() }
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
@@ -18,6 +25,7 @@ CardProduct.propTypes = {
   title: PropTypes.string.isRequired,
   thumbnail: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default CardProduct;
